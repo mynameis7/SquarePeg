391fd9f298f1068f4fe7eead70dd440de25ba445
mynameis7 <andrew.rather5@gmail.com>
2016-03-30 14:48:57 -0400

    added pixel checking method

---
+++ javascript/ReadPixels.js
@@ -0,0 +1,28 @@
+var velem = $("video");
+var frame = document.createElement("canvas");
+frame.width = velem.scrollWidth;
+frame.height = velem.scrollHeight;
+var fcontext = frame.getContext("2d");
+var is2D = false;
+
+function is2DFunc(videoElement, frameContext, frame) {
+	frameContext.drawImage(videoElement, 0, 0, frame.width, frame.height)
+	var idata = frameContext.getImageData();
+	var data = idata.data;
+	var count = 0;
+	for(var i = 0; i < data.length; i += 4) {
+		var r = data[i];
+		var g = data[i+1];
+		var b = data[i+2];
+		if(r === 0 && g === 0 && b === 0) {
+			count += 1;
+		}
+	}
+	if(count/(frame.width * frame.height) >= 0.5)
+		return true;
+	return false;
+}
+
+velem.addEventListener("play", function() {
+	is2D = is2DFunc(velem, fcontext, frame);
+})
